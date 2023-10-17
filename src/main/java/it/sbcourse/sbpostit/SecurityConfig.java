b0501bc5af6835aea52f972b3dd491e373496c04
Alfonso Domenici <alfonso.domenici@gmail.com>
2023-10-17 09:48:40 +0200

    jwt

--- src/main/java/it/sbcourse/sbpostit/SecurityConfig.java
+++ src/main/java/it/sbcourse/sbpostit/SecurityConfig.java
@@ -33,7 +33,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/**").fullyAuthenticated();
+                    auth
+                    .requestMatchers("/").permitAll()
+                    .requestMatchers("/swagger-ui.html").permitAll()
+                    .requestMatchers("/swagger-ui/**").permitAll()
+                    .requestMatchers("/v3/api-docs/**").permitAll()
+                    .requestMatchers("/**").fullyAuthenticated();
                 })
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwtAuthenticationConverterForKeycloak()));
 
